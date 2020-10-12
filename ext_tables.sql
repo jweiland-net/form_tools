f3675127e45128e6ecc3770079377ff7713e0957
E. Sokoll <esokoll@jweiland.net>
2020-10-12 13:08:31 +0200

    [TASK] SQL: Reduce size of phone field to 30

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,7 +4,7 @@
 CREATE TABLE tx_formtools_requests (
 	first_name varchar(255) DEFAULT '' NOT NULL,
 	last_name varchar(255) DEFAULT '' NOT NULL,
-	telephone varchar(255) DEFAULT '' NOT NULL,
+	telephone varchar(30) DEFAULT '' NOT NULL,
 	address varchar(255) DEFAULT '' NOT NULL,
 	city varchar(255) DEFAULT '' NOT NULL,
 	email varchar(255) DEFAULT '' NOT NULL,
