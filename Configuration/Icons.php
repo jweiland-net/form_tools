d9ff13894263de0efec677ff672930d682c34907
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-09-15 00:35:22 +0200

    [TASK] Fixes for TYPO3 13LTS

---
+++ Configuration/Icons.php
@@ -0,0 +1,10 @@
+<?php
+
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
+return [
+    'checkboxlink-icon' => [
+        'provider' => SvgIconProvider::class,
+        'source' => 'EXT:form_tools/Resources/Public/Icons/Form/Checkboxlink-icon.svg',
+    ],
+];
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,18 +1,13 @@
 <?php
 
-call_user_func(function () {
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Imaging\IconRegistry::class
-    );
-    $iconRegistry->registerIcon(
-        'Checkboxlink-icon',
-        \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
-        ['source' => 'EXT:form_tools/Resources/Public/Icons/Form/Checkboxlink-icon.svg']
-    );
+defined('TYPO3') or die;
 
+use TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask;
+
+call_user_func(function () {
     // Register tx_formtools_requests for table garbage collection task
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][\TYPO3\CMS\Scheduler\Task\TableGarbageCollectionTask::class]['options']['tables']['tx_formtools_requests'] = [
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][TableGarbageCollectionTask::class]['options']['tables']['tx_formtools_requests'] = [
         'dateField' => 'tstamp',
-        'expirePeriod' => 30
+        'expirePeriod' => 30,
     ];
 });
